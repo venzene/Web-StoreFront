19d6ecded0e3123a92cace7e050409f3a4e26eeb
Vishal Yadav <vishaltr55@gmail.com>
2024-05-04 18:24:45 +0530

    seperate file for routing

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const http = require('http');
 
-const server = http.createServer((req,res) => {
-    console.log(req.url,req.method, req.headers );
-    // process.exit();
-})
+const routes = require('./routes');
+
+console.log(routes.someText);
+
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
