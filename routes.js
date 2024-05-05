ef802335a159feaa9b845d8687484142505271b0
Vishal Yadav <vishaltr55@gmail.com>
2024-05-05 12:58:46 +0530

    few more updates and json file and nodemon config.

--- routes.js
+++ routes.js
@@ -29,6 +29,7 @@ const requestHandler = (req, res) => {
       });
     });
   }
+    
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>My First Page</title><head>');
