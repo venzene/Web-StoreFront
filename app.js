a254a3db45c711106583f0f10ed6dcaad88593a6
Vishal Yadav <vishaltr55@gmail.com>
2024-05-15 14:35:33 +0530

    Starting with Mongoose.

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+// const User = require('./models/user');
 
 const app = express();
 
@@ -20,22 +20,24 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false })); // these are middleware
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-    User.findById('66425512e50d6c1afdb5cd15')
-    .then(user => {
-        req.user = new User(user.name, user.email, user.cart, user._id);
-        next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//     User.findById('66425512e50d6c1afdb5cd15')
+//     .then(user => {
+//         req.user = new User(user.name, user.email, user.cart, user._id);
+//         next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
+mongoose.connect('mongodb+srv://mangal:mangalprasad@cluster0.xutnbhc.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0')
+.then( result => {
     app.listen(3000);
+    console.log('Connected !!')
 })
-
+.catch(err => console.log(err));
 
