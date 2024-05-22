aa30974ac1b1113a2e00ab4f3f1420c8b3346a78
Vishal Yadav <vishaltr55@gmail.com>
2024-05-23 03:02:40 +0530

    Payment Function added with stripe api

--- controllers/admin.js
+++ controllers/admin.js
@@ -171,8 +171,8 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req,res,next) => {
-    const prodId = req.body.productId;
+exports.deleteProduct = (req,res,next) => {
+    const prodId = req.params.productId;
     Product.findById(prodId)
     .then(product=> {
         if(!product){
@@ -183,12 +183,10 @@ exports.postDeleteProduct = (req,res,next) => {
     })
     .then(() => {
         console.log('Destroyed Product');
-        res.redirect('/admin/products');        
+        res.status(200).json({message: 'Success'});       
     })
     .catch(err => {
-        const error = new Error(err);
-        error.httpStatusCode= 500;
-        return next(error);
+        console.log(err);
+        res.status(500).json({message: 'Failure'});
     });
 }
-
